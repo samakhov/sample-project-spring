b91a7b15f3236e50b241b660b03516dff486439a
Asbath Biyalou-Sama <asbathou.biyalousama@univ-lille.fr>
2023-12-21 09:01:45 +0100

    add form to register a book

--- src/main/java/uk/lpmmi/archi/sample/controller/SimpleController.java
+++ src/main/java/uk/lpmmi/archi/sample/controller/SimpleController.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import uk.lpmmi.archi.sample.data.Book;
 import uk.lpmmi.archi.sample.repository.BookRepository;
 
 @Controller
@@ -21,6 +24,19 @@ public class SimpleController {
     public String homePage(Model model) {
         model.addAttribute("app2", appName);
         return "home";
+    }
+    
+    @GetMapping("/register_book")
+    public String register_book(Model model) {
+        model.addAttribute("book", new Book());
+        return "register_book";
+    }
+    
+    @PostMapping("/book")
+    public String book(@ModelAttribute Book book, Model model) {
+    	bookRepository.save(book);
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
     }
     
     @GetMapping("/books")
